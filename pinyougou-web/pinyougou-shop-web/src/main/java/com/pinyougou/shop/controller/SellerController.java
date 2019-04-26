b1ac279f906ab5adcbfcb2d97d76eef1e924448a
zqy <445311316@qq.com>
2019-04-26 11:10:30 +0800

    完成需求一:商家后台系统【修改资料】 完成需求二:商家后台系统【修改密码】

--- pinyougou-web/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
+++ pinyougou-web/pinyougou-shop-web/src/main/java/com/pinyougou/shop/controller/SellerController.java
@@ -4,11 +4,11 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.Seller;
 import com.pinyougou.service.SellerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/seller")
@@ -16,6 +16,8 @@ public class SellerController {
 
     @Reference
     private SellerService sellerService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -30,6 +32,80 @@ public class SellerController {
             e.printStackTrace();
             return false;
         }
+    };
+
+
+    //显示商家资料
+    @GetMapping("/findOne")
+    public Seller findOne(){
+        try {
+            // 获取登录用户名
+            String sellerId = SecurityContextHolder.getContext().getAuthentication().getName();
+            return sellerService.findOne(sellerId);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+
+    //商家资料修改
+    @PostMapping("/update")
+    public boolean update(@RequestBody Seller seller){
+        try {
+            // 获取安全上下文对象
+            SecurityContext context = SecurityContextHolder.getContext();
+            // 获取用户名
+            String sellerId = context.getAuthentication().getName();
+            System.out.println("loginName = " + sellerId);
+            sellerService.update(seller);
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+
+    //查询原来密码
+    @PostMapping("/findOldPassword")
+    public boolean findOldPassword(String oldPassword){
+        try {
+            // 获取安全上下文对象
+            SecurityContext context = SecurityContextHolder.getContext();
+            // 获取用户名
+            String sellerId = context.getAuthentication().getName();
+            //查询数据库加密的原密码
+            String password = sellerService.findOldPassword(sellerId);
+            System.out.println("loginName = " + sellerId);
+            //对比密码
+            boolean matches = passwordEncoder.matches(oldPassword, password);
+
+            return matches;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+
+    /** 修改密码 */
+    @PostMapping("/updatePassword")
+    public boolean updatePassword(String newPassword){
+        try {
+            // 获取安全上下文对象
+            SecurityContext context = SecurityContextHolder.getContext();
+            // 获取用户名
+            String sellerId = context.getAuthentication().getName();
+            //根据用户id修改密码
+            String password = passwordEncoder.encode(newPassword);
+            sellerService.updatePassword(sellerId, password);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
